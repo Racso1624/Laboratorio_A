4ca64d18074a29563be64b59dcbf97ea6df1737d
Racso1624 <fer10_lb@outlook.com>
2023-02-24 11:20:19 -0600

    precedence function

--- regex.py
+++ regex.py
@@ -4,5 +4,13 @@ class Regex (object):
         self.operators = []
         self.postfix_expression = self.postfixConversion()
 
+    def operatorPrecedence(character):
+        precedence = {'(' : 1, '|' : 2, '.' : 3, '?' : 4, '*' : 4, '+' : 4}
+
+        try:
+            return precedence[character]
+        except:
+            return 5
+
     def postfixConversion():
         pass
