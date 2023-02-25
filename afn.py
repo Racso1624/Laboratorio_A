cfb9008058727397055588aa6dbe25f357c8329a
Racso1624 <fer10_lb@outlook.com>
2023-02-24 23:46:50 -0600

    Se mejora clase afn

--- afn.py
+++ afn.py
@@ -5,3 +5,25 @@ class AFN(object):
     def __init__(self, regex):
         self.regex = regex
         self.postfix_expression = Regex(regex)
+        self.characters_stack = list(self.postfix_expression)
+        self.states_counter = 0
+        self.states = []
+        self.transitions = []
+        self.initial_state = []
+        self.final_state = []
+        self.symbols = []
+
+    def thompsonConstruction(self):
+        
+        character = self.characters_stack.pop()
+
+        if(character == '.'):
+            pass
+        elif(character == '|'):
+            pass
+        elif(character == '*'):
+            pass
+        elif(character == '+'):
+            pass
+        elif(character == '?'):
+            pass
